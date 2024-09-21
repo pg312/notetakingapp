c100928e307791a5a1174c32a990e46cd21067a2
pg312 <vpadmagade@gmail.com>
2024-09-21 21:50:57 +0530

    First commit

---
+++ src/main/java/com/personal/notetakingapp/NotetakingappApplication.java
@@ -0,0 +1,13 @@
+package com.personal.notetakingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetakingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetakingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/notetakingapp/NotetakingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.notetakingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotetakingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
